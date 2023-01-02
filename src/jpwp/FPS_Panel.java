188d6608b0b0b6052394d704c4e2c78be8b859b5
davee saiyajin <thed3jvid@gmail.com>
2023-01-02 22:35:18 +0100

    Added new classes and improved already existing

--- src/jpwp/FPS_Panel.java
+++ src/jpwp/FPS_Panel.java
@@ -4,6 +4,27 @@
  */
 package jpwp;
 
-public class FPS_Panel {
+import javax.swing.JPanel;
+import java.awt.*;
 
+public class FPS_Panel extends JPanel {
+
+    public int panelWidth;
+    public int panelHeight;
+    public int barHeight;
+
+    public FPS_Status fpsStatus;
+    public Font menuFont;
+    public Font alertFont;
+
+    public FPS_Panel(int width, int height) {
+        fpsStatus = new FPS_Status();
+        fpsStatus.reset();
+        menuFont = new Font("Dialog", Font.BOLD, 36);
+        alertFont = new Font("Dialog", Font.BOLD, 92);
+
+        this.panelWidth = width;
+        this.panelHeight = height;
+        barHeight = 50;
+    }
 }
